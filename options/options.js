5dcf7d09131076163e3a41409f40f1dcd6f99a62
Juan B <juan@crowdbotics.com>
2023-04-10 16:10:10 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "juan10042023-dev-75734",
-  url: "https://juan10042023-dev-75734.botics.co",
-  api: "https://juan10042023-dev-75734.botics.co/api/v1"
-}
+  "name": "juan10042023-dev-75734",
+  "url": "https://juan10042023-dev-75734.botics.co",
+  "api": "https://juan10042023-dev-75734.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
--- store/juandevAPI/api.js
+++ store/juandevAPI/api.js
@@ -1,68 +1,96 @@
-import axios from "axios"
+import axios from "axios";
 const juandevAPI = axios.create({
   baseURL: "https://juan10042023-dev-75734-prod.herokuapp.com",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_v1_cat_list(payload) {
-  return juandevAPI.get(`/api/v1/cat/`)
+  return juandevAPI.get(`/api/v1/cat/`);
 }
+
 function api_v1_cat_create(payload) {
-  return juandevAPI.post(`/api/v1/cat/`, payload.data)
+  return juandevAPI.post(`/api/v1/cat/`, payload.data);
 }
+
 function api_v1_login_create(payload) {
-  return juandevAPI.post(`/api/v1/login/`, payload.data)
+  return juandevAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_signup_create(payload) {
-  return juandevAPI.post(`/api/v1/signup/`, payload.data)
+  return juandevAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return juandevAPI.get(`/rest-auth/user/`)
+  return juandevAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return juandevAPI.put(`/rest-auth/user/`, payload.data)
+  return juandevAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return juandevAPI.patch(`/rest-auth/user/`, payload.data)
+  return juandevAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 function api_docs_schema_retrieve(payload) {
-  return juandevAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
+  return juandevAPI.get(`/api-docs/schema/`, {
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_cat_retrieve(payload) {
-  return juandevAPI.get(`/api/v1/cat/${payload.id}/`)
+  return juandevAPI.get(`/api/v1/cat/${payload.id}/`);
 }
+
 function api_v1_cat_update(payload) {
-  return juandevAPI.put(`/api/v1/cat/${payload.id}/`, payload.data)
+  return juandevAPI.put(`/api/v1/cat/${payload.id}/`, payload.data);
 }
+
 function api_v1_cat_partial_update(payload) {
-  return juandevAPI.patch(`/api/v1/cat/${payload.id}/`, payload.data)
+  return juandevAPI.patch(`/api/v1/cat/${payload.id}/`, payload.data);
 }
+
 function api_v1_cat_destroy(payload) {
-  return juandevAPI.delete(`/api/v1/cat/${payload.id}/`)
+  return juandevAPI.delete(`/api/v1/cat/${payload.id}/`);
 }
+
 function rest_auth_login_create(payload) {
-  return juandevAPI.post(`/rest-auth/login/`, payload.data)
+  return juandevAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return juandevAPI.get(`/rest-auth/logout/`)
+  return juandevAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return juandevAPI.post(`/rest-auth/logout/`)
+  return juandevAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_registration_create(payload) {
-  return juandevAPI.post(`/rest-auth/registration/`, payload.data)
+  return juandevAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return juandevAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return juandevAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_change_create(payload) {
-  return juandevAPI.post(`/rest-auth/password/change/`, payload.data)
+  return juandevAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return juandevAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+  return juandevAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return juandevAPI.post(`/rest-auth/registration/verify-email/`, payload.data)
+  return juandevAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 export const apiService = {
   api_v1_cat_list,
   api_v1_cat_create,
@@ -84,4 +112,4 @@ export const apiService = {
   rest_auth_password_change_create,
   rest_auth_password_reset_confirm_create,
   rest_auth_registration_verify_email_create
-}
+};
